ae5a8a4912417eb3e70a53958455eff3ee2cdd2f
douglas-cavalcante <henriquedouglas2803@gmail.com>
2022-04-07 19:57:45 -0300

    adiciona cors para APi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+require('dotenv').config()
+
 import express from 'express';
 import routes from './routes';
 import './database';
 import Logger from './config/logger'
 import morgan from './config/morgan'
+import cors from 'cors'
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 class App {
 
   constructor() {
     this.server = express()
+    this.setupSentry()
     this.middlewares()
     this.routes()
 
@@ -16,13 +23,30 @@ class App {
   middlewares() {
     console.log('Start middlewares')
     this.server.use(express.json())
+    this.server.use(cors())
     this.server.use(morgan)
+    this.server.use(Sentry.Handlers.requestHandler());
+    // TracingHandler creates a trace for every incoming request
+    this.server.use(Sentry.Handlers.tracingHandler());
+
+
   }
 
   routes() {
     this.server.use(routes)
   }
-  
+
+  setupSentry() {
+    Sentry.init({
+      dsn: process.env.DSN_SENTRY,
+      integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app: this.server }),
+      ],
+      tracesSampleRate: 1.0,
+    });
+  }
+
 }
 
 export default new App().server;
